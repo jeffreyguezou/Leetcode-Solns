fcbaf3d0cd1a723cc0c3376181e137e85e760579
jeffreyguezou <jeffreyrajcj@gmail.com>
2024-02-08 12:20:18 +0530

    Split function- O(n) complexity

--- Split.js
+++ Split.js
@@ -2,31 +2,39 @@ function split(str, limiter) {
   let i = 0;
 
   let subString = "";
+  let tempString = "";
   let result = [];
 
-  while (i < str.length) {
-    if (limiter.length === 0) {
+  if (limiter.length === 0) {
+    for (let i = 0; i < str.length; i++) {
       result.push(str[i]);
-    } else {
-      if (str[i] != limiter[0]) {
-        subString += str[i];
-      } else {
-        for (let j = 0; j < limiter.length; j++) {
-          if (str[i + j] != limiter[j]) {
-            subString += str[i];
-            break;
-          }
-
-          if (j === limiter.length - 1) {
-            result.push(subString);
-            subString = "";
-          }
-        }
-      }
-      if (subString && i === str.length - 1) result.push(subString);
     }
-    i++;
+    return result;
   }
+
+  while (i < str.length) {
+    if (str[i] === limiter[0]) {
+      for (let j = 0; j < limiter.length; j++) {
+        if (str[i + j] !== limiter[j]) {
+          break;
+        } else tempString += str[i + j];
+      }
+      if (tempString === limiter) {
+        result.push(subString);
+        subString = "";
+        tempString = "";
+        i = i + limiter.length;
+      } else {
+        tempString = "";
+      }
+    }
+    subString += str[i];
+    i++;
+    if (i === str.length && subString) {
+      result.push(subString);
+    }
+  }
+
   return result;
 }
 
@@ -34,3 +42,5 @@ console.log(split("JeffreyfRajffguefzffou", "ff"));
 console.log(split("abcdcgefcd", "c"));
 console.log(split("Jeffrey Raj Guezou", ""));
 console.log(split("Jeffrey Raj Guezou", " "));
+console.log(split("JeffreyfRajffguefzffou", "fr"));
+console.log(split("jeffreyraj", "xyz"));
